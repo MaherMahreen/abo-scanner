3f1d67eb1e13799f4285232c2c3582ca67e85568
MaherMahreen <teguhsetia09@gmail.com>
2026-07-24 01:49:36 +0800

    Update jalan_radar.py

--- jalan_radar.py
+++ jalan_radar.py
@@ -1,28 +1,27 @@
 def kirim_radar_telegram(pesan):
 
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{8567909596:AAFFLsu_Nh6-WCuZbb5F73cts-VUbWBaC5A
+
+}/sendMessage"
 
     payload = {
-        "chat_id": CHAT_ID,
-        "text": pesan,
-        "parse_mode": "Markdown"
+        "chat_id": 8690860489,
+        "text": pesan
     }
 
     try:
-
         response = requests.post(
             url,
             data=payload,
             timeout=20
         )
 
-        print(response.status_code)
-        print(response.text)
+        print("URL :", url)
+        print("Status :", response.status_code)
+        print("Response :", response.text)
 
         return response.status_code == 200
 
     except Exception as e:
-
-        print("Gagal kirim Telegram:", e)
-
+        print("Gagal:", e)
         return False
