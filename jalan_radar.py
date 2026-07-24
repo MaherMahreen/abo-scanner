f1b92f17911c12066d4359cefcb01bacd08b0b0d
MaherMahreen <teguhsetia09@gmail.com>
2026-07-24 17:08:39 +0800

    Fix Telegram API URL in kirim_radar_telegram

--- jalan_radar.py
+++ jalan_radar.py
@@ -8,7 +8,7 @@ TELEGRAM_TOKEN_LANGSUNG = "8567909596:AAFwit3UXmDVY7dn2qPjectOpN_1ywYeybc"
 CHAT_ID_LANGSUNG = "8690860489"
 
 def kirim_radar_telegram(pesan):
-    url = f"https://telegram.org/bot8567909596:AAFwit3UXmDVY7dn2qPjectOpN_1ywYeybc/sendMessage"
+    url = f"https://api.telegram.org/bot8567909596:AAFwit3UXmDVY7dn2qPjectOpN_1ywYeybc/sendMessage"
     payload = {"chat_id": str("8690860489"), "text": pesan}
     try:
         response = requests.post(url, json=payload, timeout=10)
