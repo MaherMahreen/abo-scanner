b38fb82ef57c0c5722bf8e7ad523b3c977a6a10c
MaherMahreen <teguhsetia09@gmail.com>
2026-07-24 10:31:12 +0800

    Update abo_scanner.py

--- abo_scanner.py
+++ abo_scanner.py
@@ -12,8 +12,8 @@ CHAT_ID_LANGSUNG = "8690860489"
 
 def kirim_radar_telegram(pesan):
     # JALUR URL DAN PAYLOAD YANG SUDAH DIPERBAIKI 100% BENAR
-    url = f"https://telegram.org{TELEGRAM_TOKEN_LANGSUNG}/sendMessage"
-    payload = {"chat_id": str(CHAT_ID_LANGSUNG), "text": pesan, "parse_mode": "Markdown"}
+    url = f"https://telegram.org/bot8567909596:AAHy8NYFG6wL7PaZ6FbYo-kElMRcH6YuRx4/sendMessage"
+    payload = {"chat_id": str(8690860489), "text": pesan, "parse_mode": "Markdown"}
     try:
         response = requests.post(url, json=payload, timeout=10)
         return response.status_code == 200
