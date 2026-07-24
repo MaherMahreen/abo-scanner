aaf4505035e17d2ef27b7658a899b5d12692b849
MaherMahreen <teguhsetia09@gmail.com>
2026-07-24 15:09:01 +0800

    Change Telegram bot token and message URL
    Updated the Telegram bot token and URL format for sending messages.

--- jalan_radar.py
+++ jalan_radar.py
@@ -4,12 +4,12 @@ import re
 import time
 from score import ScoreEngine
 
-TELEGRAM_TOKEN_LANGSUNG = "8567909596:AAHy8NYFG6wL7PaZ6FbYo-kElMRcH6YuRx4"
+TELEGRAM_TOKEN_LANGSUNG = "8567909596:AAFwit3UXmDVY7dn2qPjectOpN_1ywYeybc"
 CHAT_ID_LANGSUNG = "8690860489"
 
 def kirim_radar_telegram(pesan):
-    url = f"https://telegram.org{TELEGRAM_TOKEN_LANGSUNG}/sendMessage"
-    payload = {"chat_id": str(CHAT_ID_LANGSUNG), "text": pesan}
+    url = f"https://telegram.org/bot8567909596:AAFwit3UXmDVY7dn2qPjectOpN_1ywYeybc/sendMessage"
+    payload = {"chat_id": str("8690860489"), "text": pesan}
     try:
         response = requests.post(url, json=payload, timeout=10)
         return response.status_code == 200
